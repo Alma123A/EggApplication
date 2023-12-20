470a5af10099b3980c5beca416856e3d0c831320
Alma123A <alma.tisch1@gmail.com>
2023-12-20 21:27:13 +0200

    counter

--- app/src/main/java/com/example/eggapp/MainActivity.java
+++ app/src/main/java/com/example/eggapp/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.eggapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counter = 10;
+     TextView counterTextView = findViewById(R.id.Textview_counter);
+    counterTextView.setText(""+counter);
+    }
+
+//    public void knock(View view) {
+//        if (counter > 0) {
+//            counter--;
+//            TextView counterTextView = findViewById(R.id.Textview_counter);
+//            counterTextView.setText("" + counter);
+//            if (counter == 0) {
+//                ImageView eggImageView = findViewById(R.id.ImageView_egg);
+//                eggImageView.setImageResource(R.drawable.surprise_egg);
+//            }
+//
+//        }
+//    }
+//
+//    public void reset(View view) {
+//        counter = 10;
+//        TextView counterTextView = findViewById(R.id.Textview_counter);
+//        counterTextView.setText("");
+//        ImageView eggImageView = findViewById(R.id.ImageView_egg);
+//        eggImageView.setImageResource(R.drawable.green_egg);
     }
 }
