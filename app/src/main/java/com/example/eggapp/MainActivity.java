66c165fde3acc457a30a39ae1f7c82f4f0e3d637
Alma123A <alma.tisch1@gmail.com>
2023-12-20 22:17:23 +0200

    add counter

--- app/src/main/java/com/example/eggapp/MainActivity.java
+++ app/src/main/java/com/example/eggapp/MainActivity.java
@@ -15,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        counter = 10;
+         counter=10;
+        TextView counterTextView = findViewById(R.id.TextView_counter);
+        counterTextView.setText("" + counter);
+
     }
 }
